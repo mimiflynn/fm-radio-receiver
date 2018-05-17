75efa7648390b1c72b32e46623df8c51b92f20a5
Mimi Flynn <mimiflynn@gmail.com>
2018-05-16 21:32:21 -0400

    remote working

--- Si4703_Itsy_M0/Si4703_Itsy_M0.ino
+++ Si4703_Itsy_M0/Si4703_Itsy_M0.ino
@@ -1,6 +1,9 @@
 #include <LiquidCrystal.h>
 #include <SparkFunSi4703.h>
 #include <Wire.h>
+#include "ircodes.h"
+
+#define DEBUG true
 
 // define the pins for the LCD 
 int DB4 = 10;
@@ -33,6 +36,15 @@ int lastUpButtonState = LOW;
 unsigned long lastDownDebounceTime = 0;
 unsigned long lastUpDebounceTime = 0;
 unsigned long debounceDelay = 50;
+
+// remote
+const uint8_t IRpin = A5;
+const int MAXPULSE = 65000;
+const int NUMPULSES = 50;
+const int RESOLUTION = 20;
+const int FUZZINESS = 30;
+uint16_t pulses[NUMPULSES][2];
+uint8_t currentpulse = 0;
 
 void displayInfo();
 void listenChannelDownButton();
@@ -82,6 +94,7 @@ void loop()
   listenChannelDownButton();
   listenChannelUpButton();
   listenKeyboard();
+  listenRemote();
   
 
 //  if (timer > millis())  timer = millis();
@@ -232,3 +245,152 @@ void displayInfo()
    Serial.print(" Volume:"); Serial.println(volume);
    updateLCD();
 }
+
+void listenRemote() {
+  int numberpulses;
+  
+  numberpulses = listenForIR();
+  
+  Serial.println("Heard ");
+  Serial.print(numberpulses);
+  Serial.println("-pulse long IR signal");
+  if (IRcompare(numberpulses, IRchannelUp, sizeof(IRchannelUp)/4)) {
+    Serial.println("channel up");
+    channel = radio.seekUp();
+    displayInfo();
+  }
+  if (IRcompare(numberpulses, IRchannelDown, sizeof(IRchannelDown)/4)) {
+    Serial.println("channel down");
+    channel = radio.seekDown();
+    displayInfo();
+  }
+  delay(500);
+}
+
+boolean IRcompare(int numpulses, int Signal[], int refsize) {
+  int count = min(numpulses,refsize);
+  Serial.print("count set to: ");
+  Serial.println(count);
+  for (int i=0; i< count-1; i++) {
+    int oncode = pulses[i][1] * RESOLUTION / 10;
+    int offcode = pulses[i+1][0] * RESOLUTION / 10;
+    
+#ifdef DEBUG    
+    Serial.print(oncode); // the ON signal we heard
+    Serial.print(" - ");
+    Serial.print(Signal[i*2 + 0]); // the ON signal we want 
+#endif   
+    
+    // check to make sure the error is less than FUZZINESS percent
+    if ( abs(oncode - Signal[i*2 + 0]) <= (Signal[i*2 + 0] * FUZZINESS / 100)) {
+#ifdef DEBUG
+      Serial.print(" (ok)");
+#endif
+    } else {
+#ifdef DEBUG
+      Serial.print(" (x)");
+#endif
+      // we didn't match perfectly, return a false match
+      return false;
+    }
+    
+    
+#ifdef DEBUG
+    Serial.print("  \t"); // tab
+    Serial.print(offcode); // the OFF signal we heard
+    Serial.print(" - ");
+    Serial.print(Signal[i*2 + 1]); // the OFF signal we want 
+#endif    
+    
+    if ( abs(offcode - Signal[i*2 + 1]) <= (Signal[i*2 + 1] * FUZZINESS / 100)) {
+#ifdef DEBUG
+      Serial.print(" (ok)");
+#endif
+    } else {
+#ifdef DEBUG
+      Serial.print(" (x)");
+#endif
+      // we didn't match perfectly, return a false match
+      return false;
+    }
+    
+#ifdef DEBUG
+    Serial.println();
+#endif
+  }
+  // Everything matched!
+  return true;
+}
+
+int listenForIR(void) {
+  currentpulse = 0;
+  
+  while (1) {
+    uint16_t highpulse, lowpulse;  // temporary storage timing
+    highpulse = lowpulse = 0; // start out with no pulse length
+  
+  while (digitalRead(IRpin)) { // this is too slow!
+//    while (1 << IRpin) {
+       // pin is still HIGH
+
+       // count off another few microseconds
+       highpulse++;
+       delayMicroseconds(RESOLUTION);
+
+       // If the pulse is too long, we 'timed out' - either nothing
+       // was received or the code is finished, so print what
+       // we've grabbed so far, and then reset
+       
+       // KGO: Added check for end of receive buffer
+       if (((highpulse >= MAXPULSE) && (currentpulse != 0))|| currentpulse == NUMPULSES) {
+         printpulses();
+         return currentpulse;
+       }
+    }
+    // we didn't time out so lets stash the reading
+    pulses[currentpulse][0] = highpulse;
+  
+    // same as above
+//    while (! (_BV(IRpin))) {
+      while (! digitalRead(IRpin)) {
+       // pin is still LOW
+       lowpulse++;
+       delayMicroseconds(RESOLUTION);
+        // KGO: Added check for end of receive buffer
+        if (((lowpulse >= MAXPULSE)  && (currentpulse != 0))|| currentpulse == NUMPULSES) {
+          printpulses();
+         return currentpulse;
+       }
+    }
+    pulses[currentpulse][1] = lowpulse;
+
+    // we read one high-low pulse successfully, continue!
+    currentpulse++;
+  }
+}
+
+void printpulses(void) {
+  Serial.println("\n\r\n\rReceived: \n\rOFF \tON");
+  for (uint8_t i = 0; i < currentpulse; i++) {
+    Serial.print(pulses[i][0] * RESOLUTION, DEC);
+    Serial.print(" usec, ");
+    Serial.print(pulses[i][1] * RESOLUTION, DEC);
+    Serial.println(" usec");
+  }
+  
+  // print it in a 'array' format
+  Serial.println("int IRsignal[] = {");
+  Serial.println("// ON, OFF (in 10's of microseconds)");
+  for (uint8_t i = 0; i < currentpulse-1; i++) {
+    Serial.print("\t"); // tab
+    Serial.print(pulses[i][1] * RESOLUTION / 10, DEC);
+    Serial.print(", ");
+    Serial.print(pulses[i+1][0] * RESOLUTION / 10, DEC);
+    Serial.println(",");
+  }
+  Serial.print("\t"); // tab
+  Serial.print(pulses[currentpulse-1][1] * RESOLUTION / 10, DEC);
+  Serial.print(", 0");
+  Serial.println("};");
+}
+
